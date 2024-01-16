64995bc8fc75b80f6716bdf1cdf0bf353f2866f7
Sandwich <153335053+SandwichOriginal@users.noreply.github.com>
2024-01-15 18:52:06 -0600

    Upload 📁
    Signed-off-by: Sandwich <153335053+SandwichOriginal@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // Main Variables
 const WebMod = {
-    version: "1.0.0 LATEST",
+    version: "BETA 1",
     scripts: ["JavaScript Files/commands.js"],
     local_storage_data: {
         "preventDefault": false,
         "developerMode": false,
     },
     main_message() {
-        console.log(`%cWeb Mod v${WebMod.version}`, "margin-bottom: 10px;font-family: Arial;font-size: 40px;font-weight:600;background:linear-gradient(to right, #ef5350, #f48fb1, #7e57c2, #2196f3, #26c6da, #43a047, #eeff41, #f9a825, #ff5722);-webkit-background-clip: text;-webkit-text-fill-color: transparent;");
+        console.log(`%cWeb Mod ${WebMod.version}`, "margin-bottom: 10px;font-family: Arial;font-size: 40px;font-weight:600;background:linear-gradient(to right, #ef5350, #f48fb1, #7e57c2, #2196f3, #26c6da, #43a047, #eeff41, #f9a825, #ff5722);-webkit-background-clip: text;-webkit-text-fill-color: transparent;");
         console.log(`%cDeveloper Tools But Better. Get the Full Documentation by "help()" all lowercase.`, "color: white;font-weight: 600;");
     },
     load_scripts() {
